d175a9b4aaeb089c8b8803d3e42a51e7afef9e37
Przemysl4wR4k <50297318+Przemysl4wR4k@users.noreply.github.com>
2019-05-22 00:25:39 +0200

    odejmowanie jako analogia dodawania

---
+++ subtracting.cpp
@@ -0,0 +1,100 @@
+#include "subtracting.h"
+
+using namespace std;
+
+vector <bool> subtracting(vector <bool> vecta, vector <bool> vectb)
+{
+                         //1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
+ // vector <bool> ciag1 = {0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,1,0,1,0,0,1,1,0,0,0,1,1,0,0,0,0,1};
+ // vector <bool> ciag2 = {0,0,1,0,1,0,0,1,0,1,1,1,0,1,1,0,0,0,0,1,0,0,0,0,1,1,0,1,1,0,1,1,1,0};
+  vector <bool> pomoc;
+
+  vector <bool> partA [32];
+  vector <bool> partB [32];
+  vector <bool> wynik;
+
+cout<<"\n\njestesmy w odejmowaniu\n\n";
+  int uzup;
+  int licz=0;
+  int dlA = vecta.size();
+  int dlB = vecta.size();
+
+  bool porzyczka=0;
+  bool bitwyniku=0;
+  if (dlA > dlB)
+  {
+      if(vectb[0]==0)
+      {
+           uzup=0;
+      }
+      else uzup=1;
+      pomoc = vecta;
+
+      for(int i=0; i<dlA-dlB; i++)
+      {
+              pomoc[i]=uzup;
+      }
+      for(int i=dlA-dlB; i<dlA; i++)
+      {
+              pomoc[i]=vectb[i-dlA+dlB];
+      }
+    dlB=dlA;
+  }
+  else if (dlA < dlB)
+  {
+      if(vecta[0]==0)
+      {
+           uzup=0;
+      }
+      else uzup=1;
+      pomoc = vectb;
+
+      for(int i=0; i<dlB-dlA; i++)
+      {
+              pomoc[i]=uzup;
+      }
+      for(int i=dlB-dlA; i<dlB; i++)
+      {
+              pomoc[i]=vecta[i-dlB+dlA];
+      }
+    dlA=dlB;
+  }
+cout<<"\n\ndlugosci wyrownane\n\n";
+cout<<"size.a="<<vecta.size()<<endl;
+cout<<"size.b="<<vectb.size()<<endl;
+  while(dlA>0)
+  {
+      dlA = dlA-32;
+      licz++;
+  }
+    wynik = vecta;
+     licz--;
+cout<<"licz="<<licz<<endl;
+
+  while (licz>=0)
+  {
+      for (int i=31; i>=0; i--)
+      {
+            cout<<"odejmuje bit liczby a o wartosci:"<<vecta[licz*32+i]<<" z pozycji:"<<(licz*32+i)<<endl;
+            cout<<"od bitu liczby b o wartosci:"<<vectb[licz*32+i]<<" z pozycji:"<<(licz*32+i)<<endl;
+          bitwyniku = (vecta[licz*32+i]^vectb[licz*32+i])^porzyczka;
+
+            cout<<"bit wyniku ma wartosc:"<<bitwyniku<<endl;
+
+          porzyczka = ((~(vecta[licz*32+i]^vectb[licz*32+i]))&porzyczka)|(~(vecta[licz*32+i])&(vectb[licz*32+i]));
+
+            cout<<"porzyczka ma wartosc:"<<porzyczka<<endl;
+            cout<<"test d³ugosci wyniku:"<< wynik.size()<<endl;
+          wynik[licz*32+i] = bitwyniku;
+            cout<<"test d³ugosci wyniku:"<< wynik.size()<<endl;
+
+          //cout<<pomoc[i*licz];
+      }
+
+
+
+      licz--;
+  }
+
+    return wynik;
+}
---
+++ subtracting.h
@@ -0,0 +1,12 @@
+#ifndef SUBTRACTING_H_INCLUDED
+#define SUBTRACTING_H_INCLUDED
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+vector <bool> subtracting(vector <bool> vecta, vector <bool> vectb);
+
+
+#endif // SUBTRACTING_H_INCLUDED
