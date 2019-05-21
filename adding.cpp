961c1efcb8acb6148e8fc35c69596c40c9643faf
Przemysl4wR4k <50297318+Przemysl4wR4k@users.noreply.github.com>
2019-05-22 00:25:11 +0200

    dodawanie nie tylko ukończone, ale też działa xD

--- adding.cpp
+++ adding.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 vector <bool> adding(vector <bool> vecta, vector <bool> vectb)
 {
-                       //1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
-  vector <bool> ciag1 = {0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,1,0,1,0,0,1,1,0,0,0,1,1,0,0,0,0,1};
-  vector <bool> ciag2 = {0,0,1,0,1,0,0,1,0,1,1,1,0,1,1,0,0,0,0,1,0,0,0,0,1,1,0,1,1,0,1,1,1,0};
+                         //1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
+ // vector <bool> ciag1 = {0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,1,0,1,0,0,1,1,0,0,0,1,1,0,0,0,0,1};
+ // vector <bool> ciag2 = {0,0,1,0,1,0,0,1,0,1,1,1,0,1,1,0,0,0,0,1,0,0,0,0,1,1,0,1,1,0,1,1,1,0};
   vector <bool> pomoc;
 
   vector <bool> partA [32];
   vector <bool> partB [32];
   vector <bool> wynik;
 
-cout<<"\n\njesteœmy w dodawaniu\n\n";
+cout<<"\n\njesteÅ“my w dodawaniu\n\n";
   int uzup;
   int licz=0;
-  int dlA = ciag1.size();
-  int dlB = ciag2.size();
+  int dlA = vecta.size();
+  int dlB = vecta.size();
 
   bool przeniesienie=0;
   bool bitwyniku=0;
   if (dlA > dlB)
   {
-      if(ciag2[0]==0)
+      if(vectb[0]==0)
       {
            uzup=0;
       }
       else uzup=1;
-      pomoc = ciag1;
+      pomoc = vecta;
 
       for(int i=0; i<dlA-dlB; i++)
       {
@@ -36,18 +36,18 @@ cout<<"\n\njesteœmy w dodawaniu\n\n";
       }
       for(int i=dlA-dlB; i<dlA; i++)
       {
-              pomoc[i]=ciag2[i-dlA+dlB];
+              pomoc[i]=vectb[i-dlA+dlB];
       }
     dlB=dlA;
   }
   else if (dlA < dlB)
   {
-      if(ciag1[0]==0)
+      if(vecta[0]==0)
       {
            uzup=0;
       }
       else uzup=1;
-      pomoc = ciag2;
+      pomoc = vectb;
 
       for(int i=0; i<dlB-dlA; i++)
       {
@@ -55,27 +55,38 @@ cout<<"\n\njesteœmy w dodawaniu\n\n";
       }
       for(int i=dlB-dlA; i<dlB; i++)
       {
-              pomoc[i]=ciag1[i-dlB+dlA];
+              pomoc[i]=vecta[i-dlB+dlA];
       }
     dlA=dlB;
   }
 cout<<"\n\ndlugosci wyrownane\n\n";
-  while(dlA>=0)
+cout<<"size.a="<<vecta.size()<<endl;
+cout<<"size.b="<<vectb.size()<<endl;
+  while(dlA>0)
   {
       dlA = dlA-32;
       licz++;
   }
-    wynik = pomoc;
+    wynik = vecta;
+     licz--;
 cout<<"licz="<<licz<<endl;
-  while (licz!=0)
+
+  while (licz>=0)
   {
       for (int i=31; i>=0; i--)
       {
+            cout<<"dodaje bit liczby a o wartosci:"<<vecta[licz*32+i]<<" z pozycji:"<<(licz*32+i)<<endl;
+            cout<<"do bitu liczby b o wartosci:"<<vectb[licz*32+i]<<" z pozycji:"<<(licz*32+i)<<endl;
+          bitwyniku = (vecta[licz*32+i]^vectb[licz*32+i])^przeniesienie;
 
-          bitwyniku = (ciag1[licz*32+i]^ciag2[licz*32+i])^przeniesienie;
-          przeniesienie = ((ciag1[licz*32+i]^ciag2[licz*32+i])&przeniesienie)|(ciag1[licz*32+i]&ciag2[licz*32+i]);
+            cout<<"bit wyniku ma wartosc:"<<bitwyniku<<endl;
 
+          przeniesienie = (((vecta[licz*32+i]^vectb[licz*32+i]))&przeniesienie)|(vecta[licz*32+i]&(vectb[licz*32+i]));
+
+            cout<<"przeniesienie ma wartosc:"<<przeniesienie<<endl;
+            cout<<"test dÅ‚ugosci wyniku:"<< wynik.size()<<endl;
           wynik[licz*32+i] = bitwyniku;
+            cout<<"test dÅ‚ugosci wyniku:"<< wynik.size()<<endl;
 
           //cout<<pomoc[i*licz];
       }
