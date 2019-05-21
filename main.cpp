46a50a47b7aef0406541896a2d1b7bd1eebc0160
Przemysl4wR4k <50297318+Przemysl4wR4k@users.noreply.github.com>
2019-05-22 00:26:39 +0200

    main pozwala na uzycie dodawania i odejmowania

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include "conv.h"
 #include "adding.h"
+#include "subtracting.h"
+
 
 using namespace std;
 
@@ -11,15 +13,38 @@ char *decToBin(int);
 int main()
 {
 vector <bool> a, b;
-vector <bool> c {1,0};
-vector <bool> d {0,1};
-//a = conv();
-//b = conv();
+vector <bool> c;
+vector <bool> d;
 
-cout<<endl<<endl<<"jestesmy w mainie"<<endl;
-a = adding(c, d);
+cout<<"podaj a:"<<endl;
+
+a = conv();
+
+
+cout<<endl<<"wczytalem a:"<<endl;
 for (int i = 0; i < a.size(); i++)  cout<<a[i];
+cout<<endl;
 
+cout<<"podaj b:"<<endl;
+
+b = conv();
+
+cout<<endl<<"wczytalem b:"<<endl;
+for (int i = 0; i < b.size(); i++)  cout<<b[i];
+cout<<endl;
+
+
+c = adding(a, b);
+
+cout<<endl<<"suma a + b:"<<endl;
+for (int i = 0; i < c.size(); i++)  cout<<c[i];
+cout<<endl;
+
+d = subtracting(a, b);
+
+cout<<endl<<"roznica a + b:"<<endl;
+for (int i = 0; i < d.size(); i++)  cout<<d[i];
+cout<<endl;
 
 return 0;
 }
